cb5cd3cc6f63fefe7dcc70cea71aefb86330d670
Matheus Ferreira <matheusfeereirabispo1380@gmail.com>
2025-02-19 17:36:47 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './style'
+
+const Avatar = () => <FotoPerfil src="https://github.com/Matheus8013.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { BotaoTema, Descricao, SideBarContainer } from './style'
 
 const Sidebar = () => (
   <aside>
-    <img src="http://github.com/Matheus8013.png" alt="" />
-    <Titulo fontSize={20}>Matheus Ferreira</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Matheus Ferreira</Titulo>
+      <Paragrafo fontSize={16} tipo="secundario">
+        Matheus8013
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
