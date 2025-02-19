085973be537e43c980cd1070f0fb4c9e6adccce2
Matheus Ferreira <matheusfeereirabispo1380@gmail.com>
2025-02-19 10:57:37 -0300

    configurando o ambiente de desenvolvimento

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
+import eslintPluginPrettier from 'eslint-plugin-prettier' // Importe o plugin prettier
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+      prettier: eslintPluginPrettier // Adicione o plugin prettier
+    },
+    rules: {
+      ...pluginReactHooks.configs.recommended.rules,
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      'prettier/prettier': 'error' // Regra para exibir erros do Prettier como erros do ESLint
+    }
+  },
+  { settings: { react: { version: 'detect' } } }
+]
